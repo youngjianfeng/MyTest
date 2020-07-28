fadbd1e872ef76452cf6a593b4018d8c03a98c94
yangjianfeng <yangjianf@worldunion.com.cn>
2020-07-28 12:33:39 +0800

    Initial Commit

---
+++ MyProduct/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyProduct
+//
+//  Created by worldunionGreen on 2020/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
